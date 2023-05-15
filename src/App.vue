b33ce3f6411c2307522e043250a393fa73031277
tungth <tungth@mor.com.vn>
2023-05-15 17:07:24 +0700

    build base

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,7 @@
 
 <template>
   <div>
-    {{ routeName }}
-    <Sidebar v-if="routeName"></Sidebar>
+    <Sidebar v-if="isrouteName"></Sidebar>
     <RouterView />
   </div>
 </template>
@@ -15,14 +14,14 @@ import { useRouter } from 'vue-router'
 import { ref } from 'vue'
 import { ROUTER_NAME_AUTHENTICATION } from '@/constant/index'
 
-const routeName = ref()
+const isrouteName = ref()
 const router = useRouter()
 
 const listNoShowSideBar = Object.values(ROUTER_NAME_AUTHENTICATION)
 console.log(listNoShowSideBar)
 
 watchEffect(() => {
-  routeName.value = listNoShowSideBar.includes(router.currentRoute.value.name) ? false : true
+  isrouteName.value = listNoShowSideBar.includes(router.currentRoute.value.name) ? false : true
 })
 </script>
 <style scoped>
